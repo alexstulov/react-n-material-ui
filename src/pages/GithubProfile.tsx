a8ec20e424a0a4dd9682b8fb73c3f9d7c67734f7
Alex Stulov <kinos.pro@gmail.com>
2020-05-01 14:10:25 +0300

    Add styles and link to github profile component.

--- src/pages/GithubProfile.tsx
+++ src/pages/GithubProfile.tsx
@@ -1,21 +1,66 @@
 import React from 'react';
+import { Card, CardActionArea, CardContent, CardActions, CardMedia, Typography, Button, makeStyles, Grid } from '@material-ui/core';
 
-export default function GithubProfile() {
-    let [githubUser, setGithubUser] = React.useState({avatar_url: ''});
+type GithubUserType = {
+    avatar_url: string;
+};
+
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 345,
+    },
+    media: {
+        height: 140,
+    },
+});
+
+const GithubProfile: React.FC<{}> = () => {
+    const classes = useStyles();
+    let [githubUser, setGithubUser] = React.useState<GithubUserType>({ avatar_url: 'https://picsum.photos/200/300' });
+    let [userLink, setUserLink] = React.useState<string>('https://github.com/');
 
     (async () => {
         // read our JSON
         let response = await fetch('/user.json');
-        let user = await response.json();
+        let user: { name: string } = await response.json();
 
         // read github user
-        let githubResponse = await fetch(`https://api.github.com/users/${user.name}`);
-        setGithubUser(await githubResponse.json());
+        try{
+            const githubResponse = await fetch(`https://api.github.com/users/${user.name}`);
+            setGithubUser(await githubResponse.json());
+        } catch(error) {
+            console.log(error.message);
+        }
+        
+        setUserLink(`https://github.com/${user.name}`);
     })();
 
     return (
-        <div>
-            <img src={githubUser.avatar_url} className="promise-example-class" width="30" height="60"/>
-        </div>
+        <Grid container justify="center">
+            <Card className={classes.root}>
+            <CardActionArea>
+                <CardMedia
+                    className={classes.media}
+                    image={githubUser.avatar_url}
+                    title="Contemplative Reptile"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">Github user</Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse scelerisque ultrices varius. Mauris enim lorem, tincidunt eget eros accumsan, elementum condimentum mi.
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <a href={userLink} target="_blank" rel="noopener noreferrer">
+                    <Button size="small" color="primary">
+                        Learn More
+                    </Button>
+                </a>
+            </CardActions>
+        </Card>
+        </Grid>
     );
 }
+
+export default GithubProfile;
