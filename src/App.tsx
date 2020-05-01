0325fc283aef35c13ba241c14b9f873265239898
Alex Stulov <kinos.pro@gmail.com>
2020-05-01 15:34:25 +0300

    Rework navigation styles.

--- src/App.tsx
+++ src/App.tsx
@@ -10,59 +10,72 @@ import IconButton from '@material-ui/core/IconButton'
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import MenuIcon from '@material-ui/icons/Menu';
-import {Link} from 'react-router-dom';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
+import { Link } from 'react-router-dom';
 
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Theme, createStyles, makeStyles } from '@material-ui/core';
 
-function App() {
+const useStyles = makeStyles((theme: Theme) => createStyles({
+    appBar: {
+        marginBottom: '15px'
+    },
+    menuButton: {
+        margin: theme.spacing(1),
+    },
+    title: {
+        flexGrow: 1
+    }
+}));
+
+const App: React.FC<{}> = () => {
+    const classes = useStyles();
     const [showMenu, setShowMenu] = React.useState(false);
 
     const newRef = React.useRef<HTMLDivElement | null>(null);
-    const [anchorEl, setAnchorEl] = React.useState();
+    const [anchorEl, setAnchorEl] = React.useState<HTMLDivElement | null>();
     React.useEffect(() => {
         setAnchorEl(newRef.current);
     }, []);
 
-    const toggleMenu = () => {
-        setShowMenu(!showMenu);
-    }
-    const handleClick = () => {
-        setShowMenu(!showMenu);
-    }
 
-    const handleClose = () => {
+    const toggleMenu = () => {
         setShowMenu(!showMenu);
     }
 
     return (
         <BrowserRouter>
             <header>
-                <div ref={newRef}>
-                    <IconButton edge="start" color="inherit" aria-label="menu" onClick={toggleMenu}>
-                        <MenuIcon />
-                    </IconButton>
-                </div>
-                <AppBar title='Main menu' position='relative'>
-                    <Menu
-                        id="simple-menu"
-                        anchorEl={anchorEl}
-                        keepMounted
-                        open={showMenu}
-                        onClose={toggleMenu}
-                    >
-                        <MenuItem onClick={toggleMenu}><Link to="/promise">Promise</Link></MenuItem>
-                        <MenuItem onClick={toggleMenu}><Link to="/user-form">Multi-step form</Link></MenuItem>
-                        <MenuItem onClick={toggleMenu}><Link to="/github-profile">Github profile</Link></MenuItem>
-                    </Menu>
+                <AppBar title="Main menu" position="static" className={classes.appBar}>
+                    <Toolbar>
+                        <span ref={newRef}>
+                        <IconButton edge="start" className={classes.menuButton} onClick={toggleMenu} color="inherit" aria-label="menu">
+                            <MenuIcon />
+                        </IconButton>
+                        </span>
+                        <Menu
+                            id="simple-menu"
+                            anchorEl={anchorEl}
+                            keepMounted
+                            open={showMenu}
+                            onClose={toggleMenu}
+                        >
+                            <MenuItem onClick={toggleMenu}><Link to="/promise">Promise</Link></MenuItem>
+                            <MenuItem onClick={toggleMenu}><Link to="/user-form">Multi-step form</Link></MenuItem>
+                            <MenuItem onClick={toggleMenu}><Link to="/github-profile">Github profile</Link></MenuItem>
+                        </Menu>
+                        <Typography variant="h6" className={classes.title}>MaterialUI & TypeScript</Typography>
+                    </Toolbar>
                 </AppBar>
             </header>
             <main className="App">
                 <Switch>
-                    <Route path="/" component={UserForm} exact/>
-                    <Route path="/promise" component={PromiseComponent}/>
-                    <Route path="/user-form" component={UserForm}/>
-                    <Route path="/github-profile" component={GithubProfile}/>
-                    <Route component={Error}/>
+                    <Route path="/" component={UserForm} exact />
+                    <Route path="/promise" component={PromiseComponent} />
+                    <Route path="/user-form" component={UserForm} />
+                    <Route path="/github-profile" component={GithubProfile} />
+                    <Route component={Error} />
                 </Switch>
             </main>
         </BrowserRouter>
