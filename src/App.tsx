499f0197fcb9f7f718b352dac85b82167247c633
Alex Stulov <kinos.pro@gmail.com>
2020-06-11 10:09:02 +0300

    Bring access control back.

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,8 @@ import ListItemIcon from "@material-ui/core/ListItemIcon";
 import InboxIcon from "@material-ui/icons/MoveToInbox";
 import DraftsIcon from "@material-ui/icons/Drafts";
 import SendIcon from "@material-ui/icons/Send";
+import LogInIcon from "@material-ui/icons/LockOpen";
+import LogOutIcon from "@material-ui/icons/Lock";
 import ExpandLess from "@material-ui/icons/ExpandLess";
 import ExpandMore from "@material-ui/icons/ExpandMore";
 import StarBorder from "@material-ui/icons/StarBorder";
@@ -207,142 +209,162 @@ export default function PersistentDrawerLeft() {
   return (
     <Router history={history}>
       {/* <div className={classes.root}> */}
-        <CssBaseline />
-        <AppBar
-          position="fixed"
-          className={clsx(classes.appBar, {
-            [classes.appBarShift]: open,
-          })}
-        >
-          <Toolbar>
-            <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              onClick={handleDrawerOpen}
-              edge="start"
-              className={clsx(classes.menuButton, open && classes.hide)}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" className={classes.title}>
-              React, Material UI & TypeScript
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <Drawer
-          className={classes.drawer}
-          variant="persistent"
-          anchor="left"
-          open={open}
-          classes={{
-            paper: classes.drawerPaper,
-          }}
-        >
-          <div className={classes.drawerHeader}>
-            <IconButton onClick={handleDrawerClose}>
-              {theme.direction === "ltr" ? (
-                <ChevronLeftIcon />
-              ) : (
-                <ChevronRightIcon />
-              )}
-            </IconButton>
-          </div>
-          <Divider />
-          <List
-            component="nav"
-            aria-labelledby="nested-list-subheader"
-            subheader={
-              <ListSubheader component="div" id="nested-list-subheader">
-                General stuff
-              </ListSubheader>
-            }
-            className={classes.nestedContainer}
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}
           >
-            <ListItem button>
-              <ListItemText>
-                <Link to="/promise">Promise</Link>
-              </ListItemText>
-            </ListItem>
-            <ListItem button>
-              <ListItemText>
-                <Link to="/react-hooks">React Hooks</Link>
-              </ListItemText>
-            </ListItem>
-            <ListItem button>
-              <ListItemText>
-                <Link to="/user-form">Multi-step form</Link>
-              </ListItemText>
-            </ListItem>
-            <ListItem button>
-              <ListItemText>
-                <Link to="/github-profile">Github profile</Link>
-              </ListItemText>
-            </ListItem>
-            <ListItem button>
-              <ListItemText>
-                <Link to="/todo-list">Todo List</Link>
-              </ListItemText>
-            </ListItem>
-            {moduleMenuLists}
-          </List>
-          <List
-            component="nav"
-            aria-labelledby="nested-list-subheader"
-            style={{display: 'none'}}
-            subheader={
-              <ListSubheader component="div" id="nested-list-subheader">
-                Collapsable list items
-              </ListSubheader>
-            }
-            className={classes.nestedContainer}
-          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            React, Material UI & TypeScript
+          </Typography>
+          {currentUser && (
+            <IconButton onClick={logout}>
+              <LogOutIcon />
+            </IconButton>
+          )}
+          {!currentUser && (
+            <Link to="/login">
+              <IconButton>
+                <LogInIcon />
+              </IconButton>
+            </Link>
+          )}
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "ltr" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </div>
+        <Divider />
+        <List
+          component="nav"
+          aria-labelledby="nested-list-subheader"
+          subheader={
+            <ListSubheader component="div" id="nested-list-subheader">
+              General stuff
+            </ListSubheader>
+          }
+          className={classes.nestedContainer}
+        >
           <ListItem button>
-              <ListItemIcon>
-                <SendIcon />
-              </ListItemIcon>
-              <ListItemText primary="Sent mail" />
-            </ListItem>
-            <ListItem button onClick={handleClick}>
-              <ListItemIcon>
-                <InboxIcon />
-              </ListItemIcon>
-              <ListItemText primary="Inbox" />
-              {dropdown ? <ExpandLess /> : <ExpandMore />}
-            </ListItem>
-            <Collapse in={dropdown} timeout="auto" unmountOnExit>
-              <List component="div" disablePadding>
-                <ListItem button className={classes.nested}>
-                  <ListItemIcon>
-                    <StarBorder />
-                  </ListItemIcon>
-                  <ListItemText primary="Starred" />
-                </ListItem>
-              </List>
-            </Collapse>
-            <ListItem button>
-              <ListItemIcon>
-                <DraftsIcon />
-              </ListItemIcon>
-              <ListItemText primary="Drafts" />
-            </ListItem>
-          </List>
-          <Divider />
-        </Drawer>
-        <main className="App">
-          <div className={classes.drawerHeader} />
-          <Switch>
-            {/* <Route path="/" component={Layout} exact /> */}
-            <Route path="/" component={() => <div style={{textAlign: 'center'}}><h2>Hello</h2></div>} exact />
-            <Route path="/promise" component={PromiseComponent} />
-            <Route path="/react-hooks" component={Hooks} />
-            <Route path="/user-form" component={UserForm} />
-            <Route path="/github-profile" component={GithubProfile} />
-            <Route path="/todo-list" component={TodoList} />
-            {moduleRoutes}
-            <Route path="/login" component={LoginPage} />
-            <Route component={Error} />
-          </Switch>
-        </main>
+            <ListItemText>
+              <Link to="/promise">Promise</Link>
+            </ListItemText>
+          </ListItem>
+          <ListItem button>
+            <ListItemText>
+              <Link to="/react-hooks">React Hooks</Link>
+            </ListItemText>
+          </ListItem>
+          <ListItem button>
+            <ListItemText>
+              <Link to="/user-form">Multi-step form</Link>
+            </ListItemText>
+          </ListItem>
+          <ListItem button>
+            <ListItemText>
+              <Link to="/github-profile">Github profile</Link>
+            </ListItemText>
+          </ListItem>
+          <ListItem button>
+            <ListItemText>
+              <Link to="/todo-list">Todo List</Link>
+            </ListItemText>
+          </ListItem>
+        </List>
+        {currentUser && moduleMenuLists}
+        <List
+          component="nav"
+          aria-labelledby="nested-list-subheader"
+          style={{ display: "none" }}
+          subheader={
+            <ListSubheader component="div" id="nested-list-subheader">
+              Collapsable list items
+            </ListSubheader>
+          }
+          className={classes.nestedContainer}
+        >
+          <ListItem button>
+            <ListItemIcon>
+              <SendIcon />
+            </ListItemIcon>
+            <ListItemText primary="Sent mail" />
+          </ListItem>
+          <ListItem button onClick={handleClick}>
+            <ListItemIcon>
+              <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary="Inbox" />
+            {dropdown ? <ExpandLess /> : <ExpandMore />}
+          </ListItem>
+          <Collapse in={dropdown} timeout="auto" unmountOnExit>
+            <List component="div" disablePadding>
+              <ListItem button className={classes.nested}>
+                <ListItemIcon>
+                  <StarBorder />
+                </ListItemIcon>
+                <ListItemText primary="Starred" />
+              </ListItem>
+            </List>
+          </Collapse>
+          <ListItem button>
+            <ListItemIcon>
+              <DraftsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Drafts" />
+          </ListItem>
+        </List>
+        <Divider />
+      </Drawer>
+      <main className="App">
+        <div className={classes.drawerHeader} />
+        <Switch>
+          {/* <Route path="/" component={Layout} exact /> */}
+          <Route
+            path="/"
+            component={() => (
+              <div style={{ textAlign: "center" }}>
+                <h2>Hello</h2>
+              </div>
+            )}
+            exact
+          />
+          <Route path="/promise" component={PromiseComponent} />
+          <Route path="/react-hooks" component={Hooks} />
+          <Route path="/user-form" component={UserForm} />
+          <Route path="/github-profile" component={GithubProfile} />
+          <Route path="/todo-list" component={TodoList} />
+          {currentUser && moduleRoutes}
+          <Route path="/login" component={LoginPage} />
+          <Route component={Error} />
+        </Switch>
+      </main>
       {/* </div> */}
     </Router>
   );
