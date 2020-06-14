0e279fc40b7e3c09b3cd47bb4ab4b3ddf4065383
Alex Stulov <kinos.pro@gmail.com>
2020-06-14 22:49:07 +0300

    Set random error probability to 5%.

--- src/modules/restore/services/book-store-service.ts
+++ src/modules/restore/services/book-store-service.ts
@@ -19,7 +19,7 @@ export default class BookStoreService {
     getBooks() {
         return new Promise((resolve, reject) => {
             setTimeout(() => {
-                if (Math.random() > 0.75) {
+                if (Math.random() > 0.95) {
                     reject(new Error('smth happened'))
                 }
                 resolve(this.data)
