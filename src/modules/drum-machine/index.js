62fb0fa23ee11d83fddc9dd17b250bac4200bad6
Alex Stulov <kinos.pro@gmail.com>
2020-09-01 22:17:29 +0300

    Install and configure husky, list-staged, prettier.

--- src/modules/drum-machine/index.js
+++ src/modules/drum-machine/index.js
@@ -1,36 +1,38 @@
-import React from 'react';
-import App from './components/app';
+import React from "react";
+import App from "./components/app";
 
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
 
-import Player from './services/player';
+import Player from "./services/player";
 import { PlayerProvider } from "./components/player-context";
 
-import store from './store';
+import store from "./store";
 const playerService = new Player();
 
 const basePath = "/drum-machine";
 
 const drumMachineWrapper = () => () => {
-return <Provider store={store}>
-<PlayerProvider value={playerService}>
-  <App/>
-</PlayerProvider>
-</Provider>;
-}
+  return (
+    <Provider store={store}>
+      <PlayerProvider value={playerService}>
+        <App />
+      </PlayerProvider>
+    </Provider>
+  );
+};
 
 export default () => {
   return {
-      name: "DrumMachine",
-      routes: [
-          {
-              routeProps: {
-                  path: `${basePath}`,
-                  exact: true,
-                  component: drumMachineWrapper(),
-              },
-              name: 'Drum machine'
-          }
-      ]
-  }
-}
+    name: "DrumMachine",
+    routes: [
+      {
+        routeProps: {
+          path: `${basePath}`,
+          exact: true,
+          component: drumMachineWrapper(),
+        },
+        name: "Drum machine",
+      },
+    ],
+  };
+};
